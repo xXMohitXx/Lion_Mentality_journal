13a8f948d065433231859d9e92d92317ae137284
Mohit Manglani <68803303+xXMohitXx@users.noreply.github.com>
2025-07-02 14:55:22 +0530

    vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
-'use client';
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-  useEffect(() => {
-    router.replace("/login");
-  }, [router]);
+  redirect("/login");
   return null;
 }
